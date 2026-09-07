a5a2c83de0a5549e59999b9c81ee68f63802b385
Shun NAKAYAMA <e245749@ie.u-ryukyu.ac.jp>
2026-09-07 16:03:43 +0900

    fix: lint errors resolved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      // シングルクォートを使うとエラーにする
+      quotes: ["error", "double"],
+    },
+  },
 ]);
 
 export default eslintConfig;
